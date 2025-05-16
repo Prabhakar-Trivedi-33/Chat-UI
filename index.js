b3771ca4812c983248d91df00adab85544b0dbb4
Prabhakar Trivedi <151915946+Prabhakar-Trivedi-33@users.noreply.github.com>
2025-05-16 10:28:10 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ArthChat from './ArthChat';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ArthChat />
+  </React.StrictMode>
+);
